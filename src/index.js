18d549795cff25c26ed3e058359d677a4ff8b35c
Joshua Tu <tjh8523@gmail.com>
2021-07-17 18:55:51 +1000

    Integrate xml-js library to deal with xml format data

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
+const axios = require('axios');
+const convertXml2Json = require('xml-js');
+
 const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', async (req, res) => {
+  const result = await axios.get('https://www.nasa.gov/rss/dyn/Houston-We-Have-a-Podcast.rss');
+
+
+  const { rss: { channel } } = convertXml2Json.xml2js(result.data, { compact: true, spaces: 4 });
+  
+  console.log(channel);
+
+  res.json(channel);
 });
 
 app.get('/sort', (req, res) => {
